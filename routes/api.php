7719db428679095c33408bd054a9151adcefdbc3
AMO-GT <128721329+AMO-GT@users.noreply.github.com>
2025-01-17 20:52:09 +0100

    Verkeerde bestanden verwijderd

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-Route::get('/available-capacity', function (Request $request) {
-    return app(RestaurantController::class)->getAvailableCapacity(
-        $request->query('date'),
-        $request->query('time')
-    );
-}); 
